59e6c68286a87072e1df9ec21d82980e16460808
Justin Foell <github.cm@foell.org>
2011-03-27 06:06:20 -0500

    made work

--- twilio-blaster.php
+++ twilio-blaster.php
@@ -7,28 +7,99 @@
  * Description: This plugin allows you to blast out messages to your message list, as well as blast a post
  */
 
-if (!class_exists("TwilioBlaster"))
+class TwilioBlaster
 {	
-	class TwilioBlaster
-	{	
-		var $admin_options_name="twilio_text_blast";
-		function TwilioBlaster()
+	var $admin_options_name="twilio_text_blast";
+
+	function onAdminMenu()
+	{
+		if(is_admin())
 		{
-
-		}
-
-		function init()
-		{
-			$this->get_admin_options();
-		}
-
-
-		function print_admin_page()
-		{
+			add_menu_page('Twilio Text Blaster',
+							 'Twilio Text Blaster',
+							 'activate_plugins',
+							 'twilio_blaster',
+							 array($this, 'printAdminPage'));
 			
-			?>
-			<div class="updated"><p><strong>Post Sent</strong></p></div>
-			<?php // } ?>
+			add_meta_box('twilio_text_blast',
+						 'Send Text?',
+						 array($this, 'textCallback'),
+						 'post' );
+		}
+	}
+
+	function textCallback()
+	{
+		$twilio_nonce=wp_create_nonce('send-text-nonce');
+		echo '<input type="checkbox" name="send_twilio_text" id="send_text" /><label for="send_text">Check to send link</label> ';
+		echo '<input type="hidden" name="nonce-twilio-blaster" value="'.$twilio_nonce.'" />';
+	}
+
+	public function saveMeta()
+	{
+		$post_id=$_POST['post_ID'];
+		$nonce=$_POST['nonce-twilio-blaster'];
+		$send_text_value=$_POST['send_twilio_text'];		
+		if (wp_verify_nonce($nonce, 'send-text-nonce') && $send_text_value == 'on')
+		{
+			$this->sendPost($post_id);
+			//add_post_meta($post_id, 'twilio_text', $send_text_value);
+		}
+	}
+
+	protected function sendPost($post_id)
+	{
+		$post = get_post($post_id);
+		//die(print_r($post, TRUE));
+
+		$twilio_app_dir = dirname(__FILE__) . '/../../../twilio-app/';
+		require_once $twilio_app_dir . 'twilio-php/twilio.php';
+		require_once $twilio_app_dir . 'config.php';
+		$client = new TwilioRestClient($AccountSid, $AuthToken);
+		//connect to DB
+		$dbconnect=mysql_connect('localhost', $mysql_user, $mysql_pass);
+		if(!$dbconnect){
+			die('Could Not Connect:' .mysql_error());
+		}
+		mysql_select_db('twilio_app');
+
+		//this will get pulled out of the database of people who have signed up for SMS
+		$query="select number as number from numbers where active='1'";
+		$numbers=mysql_query($query);
+		$txt_list=array();
+		while ($row=mysql_fetch_assoc($numbers))
+		{
+			$txt_list[]=$row['number'];
+		}
+		//bit.ly api goes here
+		$login="project515";
+		$appkey="R_315ad1d93fff05076a8f9d7e053c6257";
+		$format="xml";
+		$url=$post->guid;
+		$bitly = 'http://api.bit.ly/v3/shorten?longUrl='.urlencode($url).'&login='.$login.'&apiKey='.$appkey.'&format='.$format;
+		//get the url
+		//could also use cURL here
+		$response = file_get_contents($bitly);
+		//parse depending on desired format
+		$xml = simplexml_load_string($response);
+		$link = $xml->data->url;
+		$message = "New Post: {$post->post_title} $link";
+		foreach ($txt_list as $number){
+			$response = $client->request("/$ApiVersion/Accounts/$AccountSid/SMS/Messages", 
+										 "POST", array(
+											 "To" => $number,
+											 "From" => "612-360-2696",
+											 "Body" => $message
+											 ));
+		}		
+	}
+
+	function printAdminPage()
+	{
+		if(isset($_GET['sent'])):
+		?>		
+		<div class="updated"><p><strong>Post Sent</strong></p></div>
+			<?php endif;  ?>
 			<div class=wrap>
 			<form method="post" action="<?php echo get_bloginfo('wpurl')."/twilio-app/send.php";?>">
 			<h2>Send Campaign</h2>
@@ -39,73 +110,17 @@ if (!class_exists("TwilioBlaster"))
 			</form>
 			</div>
 			<?php 	
-		}
+	}
 
 		
-	}
 }
 //end of class, begin of out-of-class-scope section
 
-if (class_exists("TwilioBlaster"))
-{
-	$o4sqTdAdd= new TwilioBlaster();
-}
-//actions and filters
-function twilio_meta_box()
-		{
-			add_meta_box('twilio_text_blast', 'Send Text?', 'twilio_text_callback' , 'post' );
-		}
+$oTwilioTextBlaster = new TwilioBlaster();
 
-function twilio_text_callback()
-{
-	$twilio_nonce=wp_create_nonce('send-text-nonce');
-	echo '<h3>Send Text?:</h3> <input type="checkbox" name="send_twilio_text"  /> ';
-	echo '<input type="hidden" name="nonce-twilio-blaster" value="'.$twilio_nonce.'" />';
-}
+add_action( 'admin_menu', array($oTwilioTextBlaster, 'onAdminMenu'));
+add_action('edit_post' , array($oTwilioTextBlaster, 'saveMeta'));
+add_action('save_post', array($oTwilioTextBlaster, 'saveMeta'));
+add_action('publish_post' , array($oTwilioTextBlaster, 'saveMeta'));
 
-function twilio_save_meta()
-{	
-	$post_id=$_POST['post_ID'];
-	$nonce=$_POST['nonce-twilio-blaster'];
-	$send_text_value=$_POST['send_twilio_text'];
-	if (wp_verify_nonce($nonce, 'send-text-nonce'))
-	{	
-		add_post_meta($post_id, 'twilio_text', $send_text_value);
-	}
-}
-
-if(!function_exists("twilio_text_blaster_ap"))
-{
-	function twilio_text_blaster_ap()
-	{
-		global $oTwilioTextBlaster;
-		if(!isset($oTwilioTextBlaster))
-		{
-			return;
-		}
-		if (function_exists('add_submenu_page'))
-		{
-			add_submenu_page('index.php', 'Twilio Text Blaster', 'Twilio Text Blaster', 'activate_plugins', 'twilio_blaster', array(&$oTwilioTextBlaster, 'print_admin_page'));
-		}
-	}
-}
-
-
-if (isset($oTwilioTextBlaster))
-{
-	//add an action to print the script out
-	add_action('activate_twilio-text-blaster/twilio-blaster.php', array(&$oTwilioTextBlaster, 'init'));
-
-	if(is_admin())
-	{
-		add_action('admin_menu' , 'twilio_text_blaster_ap');
-		add_action('admin_menu' , 'twilio_meta_box');
-		add_action('edit_post' , 'twilio_save_meta');
-		add_action('save_post', 'twilio_save_meta');
-		add_action('publish_post' , 'twilio_save_meta');
-
-	}
-
-	//add a filter to the_content to print the button out
-}
 ?>
