ad28699f470fdd8390c4d5d4b59c3decf6b70def
Mitchell Hislop <mitch@mitchellhislop.com>
2011-04-01 21:52:57 -0500

    Adding in the needed files that we need

---
+++ config.php
@@ -0,0 +1,9 @@
+<?
+$AccountSid="AC5fafc309936bdbaed48b7d00149481ef";
+$AuthToken="58194089d691ffeb92ee7cf2add060ad";
+$ApiVersion = "2010-04-01";
+$mysql_user="twilio_app";
+$mysql_pass="full_court!wp";
+$db_name="twilio_app";
+?>
+
